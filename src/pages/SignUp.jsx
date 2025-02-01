73e6bcce8ed61243ecbedfdd552a8a88ac08fa4a
Kristal Dev <KristalDev001@gmail.com>
2025-02-01 14:34:20 +0100

    signup page beggining

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from '../assets/images/logo.png'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='w-full h-screen flex'>
+        <div className="flex w-[50%] rounded-md">
+            <img src={logo} className='w-full h-full' alt="logo"/>
+        </div>
+    </div>
   )
 }
 
